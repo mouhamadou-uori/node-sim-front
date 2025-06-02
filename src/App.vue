f3a49cc564b651b9f37df1e8b2711b588c67e6e1
NDOUR <mouhamadouwally.ndour@ucad.edu.sn>
2025-06-02 23:21:55 +0000

    first commit after scafolding the project and installing chadcn-for vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import AppSidebar from '@/components/AppSidebar.vue'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar' 
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <RouterView />
+    </main>
+  </SidebarProvider>
+</template>
+
+<style scoped></style>
